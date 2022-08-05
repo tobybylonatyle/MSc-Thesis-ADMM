39520a7885ce44bda29effb5f546f631e2b5c851
Rafal (Raf) Muszalski <rafalmu127@gmail.com>
2022-08-05 16:06:44 +0100

    small things

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import numpy as np
 
 
 SOLVER_NAME = 'gurobi'
-INSTANCE_SIZE = 2
+INSTANCE_SIZE = 100
 EQUAL_PRICES = False
-MAX_ITER = 30
+MAX_ITER = 15
 TOLERANCE = 1
 
 def solve_MILP():
@@ -24,6 +24,12 @@ def solve_MILP():
 
     print(pyo.value(instance.Objective_Cost))
 
+    decision_vars ={}
+    exports_G_T, imports_G_T, exports_S_TL, imports_S_TL = helpers.extract_from_non_decomposed(instance)
+    decision_vars[0] = {'e_G': exports_G_T, 'i_G': imports_G_T, 'e_S': exports_S_TL, 'i_S': imports_S_TL }
+
+    return decision_vars
+
 def solve_LP():
     print("> Solving LP")
     solver = helpers.build_solver(SOLVER_NAME)
@@ -38,6 +44,12 @@ def solve_LP():
 
     print(pyo.value(instance.Objective_Cost))
 
+    decision_vars ={}
+    exports_G_T, imports_G_T, exports_S_TL, imports_S_TL = helpers.extract_from_non_decomposed(instance)
+    decision_vars[0] = {'e_G': exports_G_T, 'i_G': imports_G_T, 'e_S': exports_S_TL, 'i_S': imports_S_TL }
+
+    return decision_vars
+
 def solve_ADMM():
     print("> Solving ADMM")
     solver = helpers.build_solver(SOLVER_NAME)
@@ -46,22 +58,58 @@ def solve_ADMM():
     location_models = {}
     location_instances = {}
     for l in range(INSTANCE_SIZE):
-        print(f"   Creating Subproblem for Location {l}")
-        location_models[l] = helpers.build_model("SiteModel")
-        location_instances[l] = location_models[l].build_instance(instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES)
+        print(f"   Creating Subproblem for Location {l+1}")
+        location_models[l] = helpers.build_model("LocationsModel")
+        location_instances[l] = location_models[l].build_instance(instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES, site_id=l+1)
 
-        for t in location_instances[l].T:
-            location_instances[l].dualgamma[t] = 0
+        # print(pyo.value(location_instances[l].N_l_prime))
+    
+        for k in range(INSTANCE_SIZE):
+            for t in location_instances[l].T:
+                # location_instances[l].dual[t] = 2000
+                location_instances[l].dualgamma[t] = 100
+                location_instances[l].e_S_prime[t,k+1] = 0
+                location_instances[l].i_S_prime[t,k+1] = 0
+                location_instances[l].i_G[t] = 10
+                location_instances[l].e_G[t] = 10
+
+    
+    # Not needed since we will be using the "feasibility heuristic"
+    # portfolio_model = helpers.build_model("PortfolioModel")
+    # portfolio_instnace = portfolio_model.build_instance(instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES)
+    # print("THERE")
+    model = helpers.build_model("MILPModel")
+    inst  = model.build_instance(instance_size=INSTANCE_SIZE,equal_prices=EQUAL_PRICES)
 
 
-    print("HERE")
-    portfolio_model = helpers.build_model("PortfolioModel")
-    portfolio_instnace = portfolio_model.build_instance(instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES)
-    print("THERE")
+    # -- ADMM  -- 
+    iter = 0
+    while(iter < MAX_ITER):
+        print(f"iteration {iter}")
 
-    # -- END : Create |L| +1 decomposed subproblems -- 
+        for l in range(INSTANCE_SIZE):
+            result_l = solver.solve(location_instances[l])
+            exports_S_TL, imports_S_TL = helpers.extract_from_locations(location_instances[l]) # Extract data from locations supbroblem 
+            
+            #FUCK ME
 
-    # -- ADMM PART SUPER COOL LETS GO -- 
+            
+        duals = helpers.ExchangeUpdateDuals(location_instances)
+
+            
+
+        # obj_portfolio = sum(pyo.value(location_instances[1].dual[t]) * (inst.commitment_i[t]-inst.commitment_e[t]) for t in inst.T)
+        # L_iter = obj_portfolio + sum(pyo.value(location_instances[l].Objective_Cost) for l in inst.L)
+                        
+                        
+                        #???Compute current Lagrangian value
+                        # Extract decision vars
+        # Retrieve feasible solution
+        #UPDATE THE STUFF #TODO CONTINUE HERE 
+            
+        
+
+        iter += 1
     # for l in range(INSTANCE_SIZE):
     #     for t in location_models[l].T:
     #         location_models[l].i_G[t] = 0
@@ -102,6 +150,7 @@ def solve_RFL():
 
     computational_data = {'obj_cost': [], 'dualized_constraint_value' : [], 'augmentation_value': [], 'dualsT': [], 'primal_residualsT': [], 'dualgammasT' : [], 'min_obj' :[] }
 
+    decision_vars = {}
     print(" > Solving RFL")
     solver = helpers.build_solver(SOLVER_NAME)
 
@@ -113,17 +162,25 @@ def solve_RFL():
 
     # Intialize for the very first solve 
     for t in locations_instance.T:
-        locations_instance.dual[t] = 10
-        locations_instance.dualgamma[t] = 100 # Fixed dualgamma for now
+        # locations_instance.dual[t] = 2000 #Do not initialize, they have to be within the optimal window
+        locations_instance.dualgamma[t] = 5 # Fixed dualgamma for now
         locations_instance.i_G[t] = 0
         locations_instance.e_G[t] = 0
 
     for t in portfolio_instance.T:
-        portfolio_instance.dual[t] = 10
-        portfolio_instance.dualgamma[t] = 100
+        # portfolio_instance.dual[t] = 2000 #Do not initialize, they have to be within the optimal window
+        portfolio_instance.dualgamma[t] = 5
 
     iter = 0
-
+    objective_cost_original, dualized_constraint_value, augmentation_value, min_obj = 0,0,0,0
+    primal_residualsT = np.zeros(int(pyo.value(locations_instance.N_t)))
+    dualgammasT = np.zeros(int(pyo.value(locations_instance.N_t)))
+    dualsT = np.zeros(int(pyo.value(locations_instance.N_t)))
+    exports_G_T, imports_G_T = np.zeros(int(pyo.value(locations_instance.N_t))), np.zeros(int(pyo.value(locations_instance.N_t)))
+    exports_S_TL, imports_S_TL = np.zeros((int(pyo.value(locations_instance.N_t)), int(pyo.value(locations_instance.N_l)))), np.zeros((int(pyo.value(locations_instance.N_t)), int(pyo.value(locations_instance.N_l))))
+    # for t in locations_instance.T:
+    #     dualsT[t-1] = pyo.value(locations_instance.dual[t])
+    # print(dualsT)
     # SEQUENTIAL ADMM 
     while(iter < MAX_ITER):
         print(f"iteration {iter}")
@@ -149,6 +206,8 @@ def solve_RFL():
         computational_data['dualgammasT'].append(dualgammasT)
         computational_data['min_obj'].append(min_obj)
 
+        decision_vars[iter] = {'e_G': exports_G_T, 'i_G': imports_G_T, 'e_S': exports_S_TL, 'i_S': imports_S_TL }
+
         #TODO Termination Condition
 
         iter += 1 
@@ -157,9 +216,10 @@ def solve_RFL():
     computational_data['dualsT'] = np.stack(computational_data['dualsT'],axis=0)
     computational_data['primal_residualsT'] = np.stack(computational_data['primal_residualsT'],axis=0)
     computational_data['dualgammasT'] = np.stack(computational_data['dualgammasT'],axis=0)
+    
 
 
-    return computational_data, portfolio_instance, locations_instance
+    return computational_data, portfolio_instance, locations_instance, decision_vars
 
 def solve_MSL():
     """Parallel ADMM,  feed portolio_instance and locations_instance simulatniously, then """

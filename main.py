e3a41551f011f14a4a4464eb05db47ccc9b08bb0
Rafal (Raf) Muszalski <rafalmu127@gmail.com>
2022-08-08 14:06:17 +0100

    IMPORTANT: working 2 subproblem admm

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 SOLVER_NAME = 'gurobi'
 INSTANCE_SIZE = 2
 EQUAL_PRICES = False
-MAX_ITER = 15
+MAX_ITER = 7
 TOLERANCE = 1
 
 def solve_MILP():
@@ -51,6 +51,7 @@ def solve_LP():
     return decision_vars, instance, result
 
 def solve_ALR():
+    """Do not use, actually we are not maximizing lambda (dual variable) so it has a terrible lower bound."""
     print("> Solving ALR")
     solver = helpers.build_solver('gurobi')
     model = helpers.build_model('ALRModel')
@@ -86,7 +87,7 @@ def solve_ADMM():
         for k in range(INSTANCE_SIZE):
             for t in location_instances[l].T:
                 # location_instances[l].dual[t] = 2000
-                location_instances[l].dualgamma[t] = 5
+                location_instances[l].dualgamma[t] = 50
                 location_instances[l].e_S_prime[t,k+1] = 0
                 location_instances[l].i_S_prime[t,k+1] = 0
                 location_instances[l].i_G[t] = 0
@@ -160,6 +161,7 @@ def solve_RFL():
 
     print(" > Solving RFL")
     solver = helpers.build_solver(SOLVER_NAME)
+    solver.options['NonConvex'] = 2
 
     locations_model = helpers.build_model("SiteModel")
     locations_instance = locations_model.build_instance(instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES)
@@ -169,14 +171,14 @@ def solve_RFL():
 
     # Intialize for the very first solve 
     for t in locations_instance.T:
-        locations_instance.dual[t] = 0 #Do not initialize, they have to be within the optimal window
-        locations_instance.dualgamma[t] = 0.005 # Fixed dualgamma for now
+        # locations_instance.dual[t] = 1 #Do not initialize, they have to be within the optimal window
+        locations_instance.dualgamma[t] = 250 # Fixed dualgamma for now
         locations_instance.i_G[t] = 0
         locations_instance.e_G[t] = 0
 
     for t in portfolio_instance.T:
-        portfolio_instance.dual[t] = 0 #Do not initialize, they have to be within the optimal window
-        portfolio_instance.dualgamma[t] = 0.005
+        # portfolio_instance.dual[t] = 1 #Do not initialize, they have to be within the optimal window
+        portfolio_instance.dualgamma[t] = 250
         for l in portfolio_instance.L:
             portfolio_instance.i_S[t,l] = 0
             portfolio_instance.e_S[t,l] = 0
@@ -210,17 +212,28 @@ def solve_RFL():
 
         
         result_p                                                       = solver.solve(locations_instance) # Solve locations subproblem
+        # print(result_p)
         exports_S_TL, imports_S_TL, charge_TBL, discharge_TBL          = helpers.extract_from_locations(locations_instance) # Extract data from locations supbroblem 
         portfolio_instance                                             = helpers.feed_to_portfolio(portfolio_instance, exports_S_TL, imports_S_TL) # Feed locations subproblem data into portfolio subproblem
         result_l                                                       = solver.solve(portfolio_instance) # Solve portfolio subproblem
+        # print(result_l)
         exports_G_T, imports_G_T                                       = helpers.extract_from_portfolio(portfolio_instance) # Extract data from portfolio subproblem
         locations_instance                                             = helpers.feed_to_locations(locations_instance, exports_G_T, imports_G_T) # Feed portfolio subproblem data into locations subproblem
         
+
         locations_instance, portfolio_instance, dualsT, primal_residualsT, dualgammasT    = helpers.update_the_duals(locations_instance, portfolio_instance) # Update the dual variables
         objective_cost_original, dualized_constraint_value, augmentation_value, min_obj, obj_cost_locations, obj_cost_portfolio   = helpers.calculate_obj_cost(locations_instance, portfolio_instance)
      
 
         #TODO Termination Condition
+        # num_of_converged_duals = 0
+        # for t in portfolio_instance.T:
+        #     if ((dualsT[t-1] - computational_data['dualsT'][iter][t-1])/computational_data['dualsT'][iter][t-1] < 1E-2):
+        #         num_of_converged_duals += 1 
+        # if num_of_converged_duals == pyo.value(portfolio_instance.N_t) -4:
+        #     print(f"Termination Condition, iteration {iter}")
+
+
 
         iter += 1 
     # Quickly Process Computational Data: lists of lists are covnerted into a numpy matrix. 

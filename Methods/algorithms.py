a32f7125fe91da90a2714cfd55e0013f3357de78
Rafal (Raf) Muszalski <rafalmu127@gmail.com>
2022-08-12 16:37:54 +0100

    added time complexity for all algorithms

--- Methods/algorithms.py
+++ Methods/algorithms.py
@@ -167,16 +167,21 @@ def solve_two_block_ADMM(solver_name, instance_size, equal_prices, max_iter, dua
 
 def solve_modified_two_block_ADMM(solver_name, instance_size, equal_prices, max_iter, dual_gamma:int):
     """Sequential 2 block ADMM, SiteModel and PortfolioModel(USES HEURISTIC!!!) """
+    print(" > Solving Sequential MODIFIED 2 block ADMM")
 
     computational_data = {'obj_cost': [], 'dualized_constraint_value' : [], 'augmentation_value': [], 'dualsT': [], 'primal_residualsT': [], 'dualgammasT' : [], 'min_obj' :[], 'obj_cost_locations' :[], 'obj_cost_portfolio' :[] }
     decision_vars = {}
+    time_complexity = {'instantiating_single_model':[],'solving_single_model': [], 'instantiating_locations' : [], 'instantiating_portfolio': [], 'solving_locations' : [], 'solving_portfolio': [], 'algorithm_time':[], 'heuristic_time':[]}
 
-    print(" > Solving Sequential MODIFIED 2 block ADMM")
+
+    
     solver = helpers.build_solver(solver_name)
     # solver.options['NonConvex'] = 2 #THE BUG! FINALLY FIXED
 
+    instantiating_locations_start = time.perf_counter()
     locations_model = helpers.build_model("SiteModel")
     locations_instance = locations_model.build_instance(instance_size=instance_size, equal_prices=equal_prices)
+    time_complexity['instantiating_locations'] = time.perf_counter() - instantiating_locations_start
 
     # portfolio_model = helpers.build_model("PortfolioModel")
     # portfolio_instance = portfolio_model.build_instance(instance_size=instance_size, equal_prices=equal_prices)
@@ -222,13 +227,20 @@ def solve_modified_two_block_ADMM(solver_name, instance_size, equal_prices, max_
 
         decision_vars[iter] = {'e_G': exports_G_T, 'i_G': imports_G_T, 'e_S': exports_S_TL, 'i_S': imports_S_TL, 'charge_TBL': charge_TBL, 'discharge_TBL': discharge_TBL}
 
-        
+        algorithm_time_start = time.perf_counter()
+
+        solving_locations_start = time.perf_counter()
         result_p                                                       = solver.solve(locations_instance) # Solve locations subproblem
         # print(result_p)
+        time_complexity['solving_locations'].append( time.perf_counter() - solving_locations_start)
+
         exports_S_TL, imports_S_TL, charge_TBL, discharge_TBL          = helpers.extract_from_locations(locations_instance) # Extract data from locations supbroblem 
         
         # portfolio_instance                                             = helpers.feed_to_portfolio(portfolio_instance, exports_S_TL, imports_S_TL) # Feed locations subproblem data into portfolio subproblem
+        heuristic_start_time = time.perf_counter()
         imports_G_T, exports_G_T                                       = helpers.feasibility_heuristic(locations_instance, exports_S_TL, imports_S_TL)
+        time_complexity['heuristic_time'].append(time.perf_counter() - heuristic_start_time)
+
         # locations_instance = helpers.feasibility_heuristic_BARBMONSE(locations_instance)
         # print(exports_G_T)
         # print(imports_G_T)
@@ -244,7 +256,7 @@ def solve_modified_two_block_ADMM(solver_name, instance_size, equal_prices, max_
         # locations_instance, portfolio_instance, new_dualsT, primal_residualsT, dualgammasT = helpers.update_the_duals(locations_instance, portfolio_instance)
         # objective_cost_original, dualized_constraint_value, augmentation_value, min_obj, obj_cost_locations, obj_cost_portfolio   = helpers.calculate_obj_cost(locations_instance, portfolio_instance)
      
-
+        time_complexity['algorithm_time'].append(time.perf_counter() - algorithm_time_start)
         #TODO Termination Condition
        
 
@@ -257,11 +269,13 @@ def solve_modified_two_block_ADMM(solver_name, instance_size, equal_prices, max_
     computational_data['dualgammasT'] = np.stack(computational_data['dualgammasT'],axis=0)
     
     portfolio_instance= 0
-    return computational_data, portfolio_instance, locations_instance, decision_vars
+    return computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity
 
 def solve_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual_gamma:int):
     """small bug with this one somewhere. non convergant.. but thasts kinda expected so maybe no bug..."""
     print("> Solving Exchange ADMM")
+    time_complexity = {'instantiating_single_model':[],'solving_single_model': [], 'instantiating_locations' : [], 'instantiating_portfolio': [], 'solving_locations' : [], 'solving_portfolio': [], 'algorithm_time':[], 'heuristic_time':[]}
+
     solver = helpers.build_solver(solver_name)
  
     # -- Create |L| +1 decomposed subproblems -- 
@@ -269,9 +283,11 @@ def solve_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual
     location_instances = {}
     for l in range(instance_size):
         print(f"   Creating Subproblem for Location {l+1}")
+
+        instantiating_locations_start = time.perf_counter()
         location_models[l] = helpers.build_model("LocationsModel")
         location_instances[l] = location_models[l].build_instance(instance_size=instance_size, equal_prices=equal_prices, site_id=l+1)
-
+        time_complexity['instantiating_locations'].append(time.perf_counter() - instantiating_locations_start)
 
     
         for k in range(instance_size):
@@ -283,9 +299,11 @@ def solve_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual
                 location_instances[l].i_G[t] = 0
                 location_instances[l].e_G[t] = 0
 
-
+    instantiating_portfolio_start = time.perf_counter()
     portfolio_model = helpers.build_model("PortfolioModel")
     portfolio_instance = portfolio_model.build_instance(instance_size=instance_size, equal_prices=equal_prices) 
+    time_complexity['instantiating_portfolio'] = time.perf_counter() - instantiating_locations_start
+
     for t in portfolio_instance.T:
         portfolio_instance.dualgamma[t] = dual_gamma
 
@@ -320,8 +338,15 @@ def solve_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual
 
         decision_vars[iter] = {'e_G': e_G, 'i_G': i_G, 'e_S': exports_S_TL, 'i_S': imports_S_TL, 'charge_TBL': charge_TBL, 'discharge_TBL': discharge_TBL }
 
+
+        algorithm_time_start = time.perf_counter()
+
+        iter_solving_loctions = []
         for l in range(instance_size):
+            solve_locations_start = time.perf_counter()
             result_l = solver.solve(location_instances[l])
+            iter_solving_loctions.append(time.perf_counter() - solve_locations_start)
+        time_complexity['solving_locations'].append(iter_solving_loctions)
 
 
         
@@ -329,13 +354,17 @@ def solve_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual
         exports_S_TL, imports_S_TL, charge_TBL, discharge_TBL = helpers.extract_from_exchange_ADMM(location_instances)
 
         portfolio_instance = helpers.feed_to_portfolio(portfolio_instnace=portfolio_instance, exports_S_TL=exports_S_TL, imports_S_TL=imports_S_TL)
+        solve_portfolio_start = time.perf_counter()
         result_l = solver.solve(portfolio_instance)
+        time_complexity['solving_portfolio'].append(time.perf_counter() - solve_portfolio_start)
 
         e_G, i_G = helpers.extract_from_portfolio(portfolio_instance)
 
         location_instances = helpers.feed_to_exchange_ADMM(location_instances, e_G, i_G)
 
         new_dualsT, location_instances, primal_residualsT, dualgammasT = helpers.ExchangeUpdateDuals(location_instances, i_G, e_G, imports_S_TL, exports_S_TL)
+
+        time_complexity['algorithm_time'] = time.perf_counter() - algorithm_time_start
         objective_cost_original, dualized_constraint_value, augmentation_value, min_obj  = helpers.calculate_obj_cost_exchange(location_instances, e_G, i_G, portfolio_instance)
         # obj_cost_locations, obj_cost_portfolio 
      
@@ -346,11 +375,14 @@ def solve_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual
     computational_data['min_obj'] = np.stack(computational_data['min_obj'],axis=0)
     computational_data['dualgammasT'] = np.stack(computational_data['dualgammasT'],axis=0)
 
-    return computational_data, portfolio_instance, location_instances, decision_vars
+    return computational_data, portfolio_instance, location_instances, decision_vars, time_complexity
 
 
 def solve_modified_exchange_ADMM(solver_name, instance_size, equal_prices, max_iter, dual_gamma:int):
     print("> Solving MODIFIED Exchange ADMM")
+
+    time_complexity = {'instantiating_single_model':[],'solving_single_model': [], 'instantiating_locations' : [], 'instantiating_portfolio': [], 'solving_locations' : [], 'solving_portfolio': [], 'algorithm_time':[], 'heuristic_time':[]}
+
     solver = helpers.build_solver(solver_name)
  
     # -- Create |L| +1 decomposed subproblems -- 
@@ -358,9 +390,10 @@ def solve_modified_exchange_ADMM(solver_name, instance_size, equal_prices, max_i
     location_instances = {}
     for l in range(instance_size):
         print(f"   Creating Subproblem for Location {l+1}")
+        instantiating_locations_start = time.perf_counter()
         location_models[l] = helpers.build_model("LocationsModel")
         location_instances[l] = location_models[l].build_instance(instance_size=instance_size, equal_prices=equal_prices, site_id=l+1)
-
+        time_complexity['instantiating_locations'].append(time.perf_counter() - instantiating_locations_start)
 
     
         for k in range(instance_size):
@@ -409,8 +442,15 @@ def solve_modified_exchange_ADMM(solver_name, instance_size, equal_prices, max_i
 
         decision_vars[iter] = {'e_G': e_G, 'i_G': i_G, 'e_S': exports_S_TL, 'i_S': imports_S_TL, 'charge_TBL': charge_TBL, 'discharge_TBL': discharge_TBL }
 
+        algorithm_time_Start = time.perf_counter()
+
+        iter_solving_locations = []
         for l in range(instance_size):
+            solve_locations_start = time.perf_counter()
             result_l = solver.solve(location_instances[l])
+            iter_solving_locations.append(time.perf_counter() - solve_locations_start)
+
+        time_complexity['solving_locations'].append(iter_solving_locations)
 
 
         
@@ -421,7 +461,9 @@ def solve_modified_exchange_ADMM(solver_name, instance_size, equal_prices, max_i
         # result_l = solver.solve(portfolio_instance)
 
         # e_G, i_G = helpers.extract_from_portfolio(portfolio_instance)
+        heuristic_time_start = time.perf_counter()
         i_G, e_G = helpers.feasibility_heuristic(location_instances[0], exports_S_TL, imports_S_TL)
+        time_complexity['heuristic_time'].append(time.perf_counter() - heuristic_time_start)
 
         location_instances = helpers.feed_to_exchange_ADMM(location_instances, e_G, i_G)
         portfolio_instance = helpers.feed_to_portfolio_portfolio_vars(portfolio_instance, i_G, e_G)
@@ -429,6 +471,8 @@ def solve_modified_exchange_ADMM(solver_name, instance_size, equal_prices, max_i
         
 
         new_dualsT, location_instances, primal_residualsT, dualgammasT = helpers.ExchangeUpdateDuals(location_instances, i_G, e_G, imports_S_TL, exports_S_TL)
+
+        time_complexity['algorithm_time'].append(time.perf_counter() - algorithm_time_Start)
         objective_cost_original, dualized_constraint_value, augmentation_value, min_obj  = helpers.calculate_obj_cost_exchange(location_instances, e_G, i_G, portfolio_instance)
         # obj_cost_locations, obj_cost_portfolio 
      
@@ -439,5 +483,5 @@ def solve_modified_exchange_ADMM(solver_name, instance_size, equal_prices, max_i
     computational_data['min_obj'] = np.stack(computational_data['min_obj'],axis=0)
     computational_data['dualgammasT'] = np.stack(computational_data['dualgammasT'],axis=0)
 
-    return computational_data, portfolio_instance, location_instances, decision_vars
+    return computational_data, portfolio_instance, location_instances, decision_vars, time_complexity
 

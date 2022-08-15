9e25b99a3c64efa1e141f5b65a57ebf8eea1faf4
Rafal (Raf) Muszalski <rafalmu127@gmail.com>
2022-08-15 20:22:21 +0100

    not proud of this, but it does the job done quickly

---
+++ main.py
@@ -0,0 +1,70 @@
+import Methods.algorithms as alg
+import Methods.helpers as helpers
+import Results.visualize_and_check as vis_and_check
+
+
+
+
+
+# solver, instance_size, equal_prices, max_iter, dual_gamma, lambda_initial
+experiments = [["gurobi",2,False,5,150,"0"]]
+
+
+for experiment in experiments:
+
+    SOLVER_NAME = experiment[0]
+    INSTANCE_SIZE = experiment[1]
+    EQUAL_PRICES = experiment[2]
+    MAX_ITER = experiment[3]
+    DUAL_GAMMA = experiment[4]
+    LAMBDA_INIT = experiment[5]
+
+
+    algorithm = "LP"
+    computational_data = "Not Applicable"
+    decision_vars, instance, result, time_complexity = alg.solve_LP(solver_name=SOLVER_NAME, instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES)
+    run = [computational_data, decision_vars, time_complexity]
+    path = f"instance_{INSTANCE_SIZE}/{algorithm}"
+    vis_and_check.save_run(run,path)
+    decision_vars, instance, result, time_complexity = 0, 0, 0, 0 #clear memory
+
+    algorithm = "MILP"
+    computational_data = "Not Applicable"
+    decision_vars, instance, result, time_complexity = alg.solve_MILP(solver_name=SOLVER_NAME, instance_size=INSTANCE_SIZE, equal_prices= EQUAL_PRICES)
+    run = [computational_data, decision_vars, time_complexity]
+    path = f"instance_{INSTANCE_SIZE}/{algorithm}"
+    vis_and_check.save_run(run,path)
+    decision_vars, instance, result, time_complexity = 0, 0, 0, 0 #clear memory
+
+    algorithm = "ADMM"
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = alg.solve_two_block_ADMM(solver_name=SOLVER_NAME, instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES, max_iter=MAX_ITER, dual_gamma=DUAL_GAMMA, lambda_init = LAMBDA_INIT)
+    run = [computational_data, decision_vars, time_complexity]
+    path = f"instance_{INSTANCE_SIZE}/{algorithm}_rho_{DUAL_GAMMA}_iter_{MAX_ITER}_lambdaInitial_{LAMBDA_INIT}"
+    vis_and_check.save_run(run,path)
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = 0, 0, 0, 0 ,0 #clear memory
+
+    algorithm ="mADMM"
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = alg.solve_modified_two_block_ADMM(solver_name=SOLVER_NAME, instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES, max_iter=MAX_ITER, dual_gamma=DUAL_GAMMA, lambda_init =LAMBDA_INIT)
+    run = [computational_data, decision_vars, time_complexity]
+    path = f"instance_{INSTANCE_SIZE}/{algorithm}_rho_{DUAL_GAMMA}_iter_{MAX_ITER}_lambdaInitial_{LAMBDA_INIT}"
+    vis_and_check.save_run(run,path)
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = 0, 0, 0, 0 ,0 #clear memory
+
+    algorithm ="eADMM"
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = alg.solve_exchange_ADMM(solver_name=SOLVER_NAME, instance_size=INSTANCE_SIZE, equal_prices = EQUAL_PRICES, max_iter=MAX_ITER, dual_gamma=DUAL_GAMMA, lambda_init =LAMBDA_INIT)
+    run = [computational_data, decision_vars, time_complexity]
+    path = f"instance_{INSTANCE_SIZE}/{algorithm}_rho_{DUAL_GAMMA}_iter_{MAX_ITER}_lambdaInitial_{LAMBDA_INIT}"
+    vis_and_check.save_run(run,path)
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = 0, 0, 0, 0 ,0 #clear memory
+
+
+    algorithm ="meADMM"
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = alg.solve_modified_exchange_ADMM(solver_name=SOLVER_NAME, instance_size=INSTANCE_SIZE, equal_prices=EQUAL_PRICES, max_iter=MAX_ITER, dual_gamma=DUAL_GAMMA, lambda_init =LAMBDA_INIT)
+    run = [computational_data, decision_vars, time_complexity]
+    path = f"instance_{INSTANCE_SIZE}/{algorithm}_rho_{DUAL_GAMMA}_iter_{MAX_ITER}_lambdaInitial_{LAMBDA_INIT}"
+    vis_and_check.save_run(run,path)
+    computational_data, portfolio_instance, locations_instance, decision_vars, time_complexity = 0, 0, 0, 0 ,0 #clear memory
+
+
+
+
